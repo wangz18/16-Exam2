3a737cb082e1d49368b47b7f4c030a2630c671f7
wangz18 <wangz18@rose-hulman.edu>
2018-04-17 21:42:03 -0400

    p2

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Zhiyu Wang.  March 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -154,10 +154,17 @@ def problem2(n, seq):
       :type seq:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    list = []
+    for k in range(len(seq)):
+        if len(list) < 3:
+            if seq[k] < n:
+                list = list + [seq[k]]
+        else:
+                return list
+    return 'Too few'
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
